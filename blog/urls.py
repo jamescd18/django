3b8070f540d95303448dc31b3720eb13f16550bd
James C-D <james.changdavidson@gmail.com>
2019-12-23 00:21:49 -0800

    Convert home to ListView. Add DetailView for all posts.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 # File to contain all url traffic directing for the Blog app
 # Based on the django_project urls.py file starter
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+	path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+				# pk is primary key, specified to be an integer
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post #in current package so '.' works
 
 # func : HttpRequest -> HttpResponse
@@ -10,6 +11,16 @@ def home(request):
         'posts': Post.objects.all() # Grabs list of all posts
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+	context_object_name = 'posts'
+	ordering = ['-date_posted'] #'-' indicates newest to oldest
+
+class PostDetailView(DetailView):
+	model = Post
+	context_object_name = 'post'
 
 def about(request):
     context = {
