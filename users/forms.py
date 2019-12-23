ddd0533f7e3fe6280d2ffb547bf590787b979505
James C-D <james.changdavidson@gmail.com>
2019-12-22 23:34:07 -0800

    Added forms to properly update profile information.

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
-	#fname = forms.Text()
-	#lname = forms.Text()
-	#age = forms.DatePicker()
+	first_name = forms.Field()
+	last_name = forms.Field()
 
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'password1', 'password2']#'fname', 'lname', 'age', 
+		fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField()
+
+	class Meta:
+		model = User
+		fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['bio'] # 'image'
