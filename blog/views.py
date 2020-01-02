d4196baad4cf400149182db6792a29ca41337365
James C-D <james.changdavidson@gmail.com>
2020-01-02 22:20:34 +0800

    Added pagination to main page. Fixed buttons, changed updoot button.

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,7 @@ class PostListView(ListView):
 	template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
 	context_object_name = 'posts'
 	ordering = ['-updoots', '-date_posted'] #'-' indicates newest to oldest
+	paginate_by = 5
 
 class PostDetailView(DetailView):
 	model = Post
